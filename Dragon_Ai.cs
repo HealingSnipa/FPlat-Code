ff820953db134eaf481df6c7e08c3524f85968c4
HealingSnipa <wilkessmith@gmail.com>
2025-11-12 09:57:04 -0500

    Add files via upload

---
+++ Dragon_Ai.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Dragon_Ai : MonoBehaviour
+{
+
+
+    //variable
+    public int maxHealth;
+    private int health;
+    public int maxCooldown;
+    private int cooldown;
+    public int damageFar;
+    public int farAttackSpeed;
+    public int damageClose;
+    public int speed;
+    public int reactionTime;
+
+    public int attackDistance;
+
+    public GameObject sightline;
+    public GameObject attackFar;
+    public GameObject attackClose;
+    private int scale;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
